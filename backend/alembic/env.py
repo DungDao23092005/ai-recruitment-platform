b124c2dd10c7404109aa328a97a16464050598bc
DungDao23092005 <dungdao108@gmail.com>
2026-08-12 17:00:05 +0700

    feat(database): add initial alembic migration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -15,7 +15,10 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.database_uri)
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_uri.replace("%", "%%"),
+)
 
 target_metadata = Base.metadata
 
